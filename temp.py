163ed4fbdc4eb5733d102f02342c0fff0f211e77
Xurshid Sodirov Ziyodjon O'g'li <105376084+guroba0@users.noreply.github.com>
2022-05-11 22:47:03 +0500

    create smth but i don't know
    i told about this

---
+++ temp.py
@@ -0,0 +1,125 @@
+cars = ['matiz', 'malibu','damas','spark','epicaca']
+comand = ('ssss','sssaassa','sdsdas')
+
+car = ['qalsan']
+
+    
+    
+    #mehmonlar =[ 'Jamshid', 'Sirojiddin', 'Biloliddin', 'Nodira']
+    #print("Assalomu aleykum ", mehmonlar[3],"Sizni lutfan taklif etamiz")
+    
+    
+    #for mehmonjon in mehmonlar:
+        #print("Assalomu aleykum", mehmonjon,"Sizni lutfan taklif etamiz")
+    
+    
+    
+
+#notebooks = []
+#print("O'zingiz hohlagan notebookni tanlela")
+#for mina in range(4):
+   # notebooks.append(input(f"{mina+1}-Modelini yozing"))
+#print(notebooks)
+
+
+
+
+#avtolar = ['Matiz','Gentra','Cobalt','Damas','bmw','Spark']
+#for sato in avtolar :
+  #  if sato == 'bmw':
+       # print(sato.upper())
+   # else:  
+       # print(sato.title())
+        
+#son = -33
+#if son<0 :
+#    print("manfiy")
+#else:
+#   print("Musbat")
+    
+
+
+
+
+#yosh = int(input("Assalomu aleykum Yoshingiz nechida"))
+#if yosh <= 8:
+#        narh = 0
+#elif yosh <= 15: 
+ #       narh =  500
+#else:
+ #   narh = 1000
+    
+#print(f"siz uchun {narh} so'm")
+
+
+
+#kun = input("Assalomu aleykum Bugun kun nima>>>")
+#if kun.lower()== 'shanba' or kun.lower()== 'yakshanba':
+#            print('Dam olish kuni')
+#else:
+ #          print('Bugun ish kuni')
+
+#kun = input('Assalomu aleykum bugun kun nima>>>')
+#harorat = float(input('Va nechi gradus>>>'))  
+#if kun.lower() == 'yakshanba' or kun.lower() =='shanba' and harorat >= 30:
+ #   print('Ketik chomilgani ')
+#elif kun.lower() == 'yakshanba' and harorat <= 30:
+#    print('Qoyaqo')
+#else:
+#    print('uyda o\'tir') 
+
+#menu = ['osh','qozonkabob','somsa','honim', 'Karam sho\'rva']
+#ovqat = input('Assalomu aleykum nima ovqat yeysiz ?')
+#if ovqat.lower() in menu:
+ #   print (f"Sizning {ovqat}Buyurtmangizni hal qilamiz")
+#else:
+  #  print(f"{ovqat} Yo\'qosh")
+        
+        
+menu = ['osh','qozonkabob','somsa','honim', 'Karam sho\'rva']
+buyurtmalar = []
+  
+
+if buyurtmalar:
+    print(f"Buyurtmalar ichida {len(buyurtmalar)} ta taom boor")
+else:
+    print('Tekin tamoq faqat wi fi dan foydalnishga kegan')
+
+for ddd in buyurtmalar :
+    if ddd in menu :
+        print(f"oka hozir {ddd} yes qilamiz ")
+    else:
+        print('Brat mani kechirin')
+        
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
